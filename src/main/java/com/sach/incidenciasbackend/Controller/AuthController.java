3973f6c62fc10c66665b18704a3bc0ad716f0e21
Omar-Atriano <omar.atrianova@udlap.mx>
2023-09-18 19:14:21 -0600

    CORS

--- src/main/java/com/sach/incidenciasbackend/Controller/AuthController.java
+++ src/main/java/com/sach/incidenciasbackend/Controller/AuthController.java
@@ -6,6 +6,7 @@ import com.sach.incidenciasbackend.Auth.Request.LoginRequest;
 import com.sach.incidenciasbackend.Auth.Request.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,17 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping(value = "login")
+    @PostMapping(value = "/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request)
     {
         return ResponseEntity.ok(authService.login(request));
     }
 
-    @PostMapping(value = "register")
+    @PostMapping(value = "/register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request)
     {
         return ResponseEntity.ok(authService.register(request));
